8ae725b9f3fc09bbf492b60042aa35723d1ba02c
LauraCristinaCoelho <lsantosc7@gmail.com>
2024-03-05 16:59:21 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
+const convertButton = document.querySelector(".convert-button");
+const btnReset = document.querySelector('.btnReset'); 
+const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
+const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+const currencyValueConverted = document.querySelector(".currency-value");
 
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+const convertValues = async () => {
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
 
-    const currencyValueConverted = document.querySelector(".currency-value") 
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL").then(response => response.json())
 
-    console.log(currencySelect.value)
+    const dolarToday = data.USDBRL.high
+    const euroToday =  data.EURBRL.high
+    const libraToday = data.GBPBRL.high
+    const bitcoinToday = data.BTCBRL.high
 
-    const dolarToday = 4.96
-    const euroToday = 5.34
-    const libraToday = 6.25
-    const bitcoinToday = 214452.63
 
-    if (currencySelect.value == "dolar") { 
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue);
+
+    if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolarToday);
 
     }
-    if (currencySelect.value == "euro") { 
+    if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euroToday);
     }
 
-    if (currencySelect.value == "libra") { 
+    if (currencySelect.value == "libra") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
             style: "currency",
             currency: "GBP"
-        }).format(inputCurrencyValue / libraToday)
+        }).format(inputCurrencyValue / libraToday);
     }
 
     if (currencySelect.value == "bitcoin") {
@@ -41,18 +47,13 @@ function convertValues() {
             style: 'currency',
             currency: 'BTC',
             minimumFractionDigits: 8
-        }).format(inputCurrencyValue / bitcoinToday)
+        }).format(inputCurrencyValue / bitcoinToday);
     }
+};
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputCurrencyValue)
-}
-
-function changeCurrency() {
-    const currencyName = document.getElementById("currency-name")
-    const currencyImage = document.querySelector(".currency-img")
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name");
+    const currencyImage = document.querySelector(".currency-img");
 
     if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar Americano"
@@ -75,7 +76,13 @@ function changeCurrency() {
     }
 
     convertValues()
-}
+};
 
-currencySelect.addEventListener("change", changeCurrency)
-convertButton.addEventListener("click", convertValues)
+btnReset.addEventListener('click', function() {
+    currencyValueToConvert.innerHTML  = '--';
+    currencyValueConverted.innerHTML  = '--';
+    document.querySelector(".input-currency").value = '';
+});
+
+currencySelect.addEventListener("change", changeCurrency);
+convertButton.addEventListener("click", convertValues);
